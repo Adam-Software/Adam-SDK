e166284544ee45d157eb8f94b3180834b076ed1e
Alexey Nesterov <vertigra.developer@gmail.com>
2022-09-27 16:29:00 +1000

    Update: SyncWriteServos.py

--- SyncWriteServos.py
+++ SyncWriteServos.py
@@ -3,66 +3,54 @@
 # Be sure that SCServo(STS/SMS/SCS) properties are already set as %% ID : 1 / Baudnum : 6 (Baudrate : 1000000)
 #
 
-#from scservo_sdk import *                    # Uses SCServo SDK library
+from scservo_sdk import *  # Uses SCServo SDK library
 
 # Control table address
-ADDR_STS_GOAL_POSITION     = 42
-ADDR_STS_GOAL_SPEED        = 46
+ADDR_STS_GOAL_POSITION = 42
+ADDR_STS_GOAL_SPEED = 46
 
 # Default setting
-BAUDRATE                    = 1000000           # SCServo default baudrate : 1000000
-DEVICENAME                  = '/dev/ttyUSB0'    # Check which port is being used on your controller
-                                                # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
-
-#SCS_MOVING_SPEED            = 2000              # SCServo moving speed
-protocol_end                = 0                 # SCServo bit end(STS/SMS=0, SCS=1)
+BAUDRATE = 1000000  # SCServo default baudrate : 1000000
+DEVICENAME = '/dev/ttyUSB0'  # Check which port is being used on your controller
+# ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+protocol_end = 0  # SCServo bit end(STS/SMS=0, SCS=1)
 
 # Initialize PortHandler instance
 # Set the port path
 # Get methods and members of PortHandlerLinux or PortHandlerWindows
-
-#portHandler = PortHandler(DEVICENAME)
-
+portHandler = PortHandler(DEVICENAME)
 # Initialize PacketHandler instance
 # Get methods and members of Protocol
-
-#packetHandler = PacketHandler(protocol_end)
-
+packetHandler = PacketHandler(protocol_end)
 # Initialize GroupSyncWrite instance
-
-#groupSyncWrite = GroupSyncWrite(portHandler, packetHandler, ADDR_STS_GOAL_POSITION, 2)
+groupSyncWrite = GroupSyncWrite(portHandler, packetHandler, ADDR_STS_GOAL_POSITION, 2)
 
 
 class SyncWriteServos():
 
     def __init__(self):
         super().__init__()
-      
+
     def SyncWriteData(self, ServosID, ServosSpeed, GoalPos):
+        # Open port
+        portHandler.openPort()
 
         i = len(ServosSpeed)
         for num in range(i):
             SCS1_ID = ServosID[num]
             SCS_MOVING_SPEED = ServosSpeed[num]
-            # SCServo#1 speed
-            result = [SCS1_ID, SCS_MOVING_SPEED]
-            print(result)
-            #scs_comm_result, scs_error = packetHandler.write2ByteTxRx(portHandler, SCS1_ID, ADDR_STS_GOAL_SPEED, SCS_MOVING_SPEED)
+            scs_comm_result, scs_error = packetHandler.write2ByteTxRx(portHandler, SCS1_ID, ADDR_STS_GOAL_SPEED,
+                                                                      SCS_MOVING_SPEED)
 
-        i = len(GoalPos) 
+        i = len(GoalPos)
         for num in range(i):
             SCS1_ID = ServosID[num]
             scs_goal_position = int(GoalPos[num])
-            param = [SCS1_ID, scs_goal_position]
-            print(param)
-            #scs_addparam_result = groupSyncWrite.addParam(SCS1_ID, scs_goal_position)
-            
-        #scs_comm_result = groupSyncWrite.txPacket()
-        # Clear syncwrite parameter storage
-        #groupSyncWrite.clearParam()
-        
-        #return scs_comm_result, scs_error, scs_addparam_result
+            param_goal_position = [SCS_LOBYTE(scs_goal_position), SCS_HIBYTE(scs_goal_position)]
+            scs_addparam_result = groupSyncWrite.addParam(SCS1_ID, param_goal_position)
 
-      
-     # Close port
-     # portHandler.closePort()
+        scs_comm_result = groupSyncWrite.txPacket()
+        # Clear syncwrite parameter storage
+        groupSyncWrite.clearParam()
+
+        return scs_comm_result, scs_error, scs_addparam_result
