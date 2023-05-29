cad28a7390087414c9bfb8b9723d2c46b63cafe4
Editscode <38367082+Editscode@users.noreply.github.com>
2023-05-29 10:47:48 +0300

    Update Motor.py

--- Models/Motor.py
+++ Models/Motor.py
@@ -7,7 +7,7 @@ class Motor:
     target_position: float
     present_position: int
 
-    def __init__(self, name: str, target_position: float, JointController: Any) -> None:
+    def __init__(self, name: str, JointController: Any, target_position: float = 0) -> None:
         self.name = name
         self.JointController = JointController
         self.target_position = target_position
