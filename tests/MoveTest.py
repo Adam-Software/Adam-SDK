e1788a2b4434a6dcc44bcd7368f63d075f9afc0a
vertigra <vertigra.developer@gmail.com>
2023-05-29 23:43:40 +1000

    Added move test

---
+++ tests/MoveTest.py
@@ -0,0 +1,50 @@
+import sys
+import os
+
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from AdamController import AdamController
+from Models.MotorCommand import MotorCommand
+from Models.SerializableCommands import SerializableCommands
+import time
+
+def HandCalculate(west: str, angle1: float, angle2: float, angle3: float):
+    return SerializableCommands([MotorCommand(west + "_shoulder", angle1),
+                                 MotorCommand(west + "_upper_arm", angle2),
+                                 MotorCommand(west + "_lower_arm_up", angle3)])
+
+
+if __name__ == '__main__':
+
+    adamController = AdamController()
+    linear_velocity = (1.0, 0.0)  # движение вперед по оси X
+    angular_velocity = 0.0         # без вращения вокруг оси Z
+
+    adamController.Move(linear_velocity, angular_velocity)
+
+    #Выставление в дефолтную позицию
+    #adamController.HandleCommand(commands=SerializableCommands(
+    #    [MotorCommand('head', 100),
+    #     MotorCommand('neck', 100),
+    #     MotorCommand('right_upper_arm', 10),
+    #     MotorCommand('left_upper_arm', 10),
+    #     MotorCommand('chest', 50),
+    #     MotorCommand('left_upper_leg', 10),
+    #     MotorCommand('right_upper_leg', 10)]))
+
+    #time.sleep(5)
+
+    #adamController2 = AdamController()
+    #adamController2.HandleCommand(commands=HandCalculate("left", 0, 65, 0))
+
+    #time.sleep(5)
+
+    #adamController3 = AdamController()
+    #adamController3.HandleCommand(commands=HandCalculate("right", 0, 65, 0))
+    
+    #time.sleep(5)
+
+    #возврат в дефолтную позицию
+    #adamController.Reset()
