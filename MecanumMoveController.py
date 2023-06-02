1cfc688d429847a75ff3680e8cf644b5e1cd56f3
Editscode <38367082+Editscode@users.noreply.github.com>
2023-06-02 09:05:25 +0300

    Update MoveController

--- MecanumMoveController.py
+++ MecanumMoveController.py
@@ -1,120 +1,71 @@
-from nis import match
-from typing import List, Tuple
-from Models.IMoveController import IMoveController
-from Models.Motor import Motor
-import pymodbus
-import serial
-from pymodbus.pdu import ModbusRequest
-from pymodbus.client.serial import ModbusSerialClient as ModbusClient #initialize a serial RTU client instance
-from pymodbus.transaction import ModbusRtuFramer
+
+from typing import Tuple
+
+from pymodbus.client.serial import ModbusSerialClient as ModbusClient
+
 
 class MecanumWheelMotor:
-    def __init__(self, client, address, directionAddress, speedAddress):
+    def __init__(self, client: ModbusClient, address: int, direction_address: int, speed_address: int, inverse: bool = False):
         self.address = address
-        self.directionAddress = directionAddress
-        self.speedAddress = speedAddress
+        self.direction_address = direction_address
+        self.speed_address = speed_address
         self.speed = 0
         self.direction = 0
         self.client = client
+        self.inverse = inverse
 
-    def setSpeed(self, speed):
-        if (self.speed == 0 and speed != 0):
-            self.client.write_registers(self.speedAddress, 1023, self.address)
-            self.client.write_registers(self.speedAddress, speed , self.address)
-        elif speed == 0:
-            self.client.write_registers(self.speedAddress, speed, self.address)
-        elif self.speed != speed:
-            self.client.write_registers(self.speedAddress, speed, self.address)
+    def set_speed(self, speed: float) -> None:
+        self.set_direction(speed >= 0)
+        if self.speed == 0 and speed != 0:
+            self.client.write_registers(self.speed_address, 1023, self.address)
+        self.client.write_registers(
+            self.speed_address, int(abs(speed)), self.address)
         self.speed = speed
 
-    def setDirection(self, direction):
-        self.direction = direction
-        self.client.write_registers(
-            self.directionAddress, self.direction, self.address)
-    
-    def getRegisters(self):
+    def set_direction(self, direction: bool) -> None:
+        if self.inverse:
+            direction = not direction
+        if (self.direction != direction):
+            self.direction = direction
+            self.client.write_registers(
+                self.direction_address, int(direction), self.address)
+
+    def get_registers(self) -> None:
         read_reg = self.client.read_input_registers(0, 10, self.address)
         print(read_reg.registers)
-        
-class MecanumMoveController(IMoveController):
-    def __init__(self) -> None:
+
+
+class MecanumMoveController:
+    def __init__(self):
         client = ModbusClient(method="rtu", port="/dev/ttyUSB1",
-                      stopbits=1, bytesize=8, parity='N', baudrate=9600)
+                              stopbits=1, bytesize=8, parity='N', baudrate=9600)
         client.connect()
 
-        self.front_left = MecanumWheelMotor(
-            client=client,
-            address=22,
-            directionAddress=2,
-            speedAddress=3
-        )
-        self.front_right = MecanumWheelMotor(
-            client=client,
-            address=23,
-            directionAddress=0,
-            speedAddress=1
-        )
-        self.rear_left = MecanumWheelMotor(
-            client=client,
-            address=22,
-            directionAddress=0,
-            speedAddress=1
-        )
-
-        self.rear_right = MecanumWheelMotor(
-            client=client,
-            address=23,
-            directionAddress=2,
-            speedAddress=3
-        )
+        self.front_left = MecanumWheelMotor(client, 22, 2, 3)
+        self.front_right = MecanumWheelMotor(client, 23, 0, 1, True)
+        self.rear_left = MecanumWheelMotor(client, 22, 0, 1)
+        self.rear_right = MecanumWheelMotor(client, 23, 2, 3, True)
 
     def move(self, linear_velocity: Tuple[float, float], angular_velocity: float) -> None:
         vx, vy = linear_velocity
         wz = angular_velocity
 
-        front_left_speed = vx - vy - wz
-        front_right_speed = vx + vy + wz
-        rear_left_speed = vx + vy - wz
-        rear_right_speed = vx - vy + wz
+        front_left_speed = vx + vy + wz
+        front_right_speed = vx - vy - wz
+        rear_left_speed = vx - vy + wz
+        rear_right_speed = vx + vy - wz
 
-        max_speed = max(abs(front_left_speed), abs(front_right_speed), abs(rear_left_speed), abs(rear_right_speed))
-        #print(linear_velocity,angular_velocity)
+        max_speed = max(abs(front_left_speed), abs(front_right_speed), abs(
+            rear_left_speed), abs(rear_right_speed))
 
+        # normalize_speed
         if max_speed > 1:
             front_left_speed /= max_speed
             front_right_speed /= max_speed
             rear_left_speed /= max_speed
             rear_right_speed /= max_speed
-        #print("front_left_speed ",front_left_speed,"front_right_speed ",front_right_speed,"rear_left_speed ",rear_left_speed,"rear_right_speed ",rear_right_speed)
 
-        if(front_left_speed >= 0):
-            self.front_left.setDirection(True)
-            self.front_left.setSpeed(int(abs(front_left_speed)*1023))
-        else:
-            self.front_left.setDirection(False)
-            self.front_left.setSpeed(int(abs(front_left_speed)*1023))
-
-        if(front_right_speed >= 0):
-            self.front_right.setDirection(False)
-            self.front_right.setSpeed(int(abs(front_right_speed)*1023))
-        else:
-            self.front_right.setDirection(True)
-            self.front_right.setSpeed(int(abs(front_right_speed)*1023))
-
-        if(rear_left_speed >= 0):
-            self.rear_left.setDirection(True)
-            self.rear_left.setSpeed(int(abs(rear_left_speed)*1023))
-        else:
-            self.rear_left.setDirection(False)
-            self.rear_left.setSpeed(int(abs(rear_left_speed)*1023))
-        
-        if(rear_right_speed >= 0):
-            self.rear_right.setDirection(False)
-            self.rear_right.setSpeed(int(abs(rear_right_speed)*1023))
-        else:
-            self.rear_right.setDirection(True)
-            self.rear_right.setSpeed(int(abs(rear_right_speed)*1023))
-
-
-
-
+        self.front_left.set_speed(front_left_speed * 1023)
+        self.front_right.set_speed(front_right_speed * 1023)
+        self.rear_left.set_speed(rear_left_speed * 1023)
+        self.rear_right.set_speed(rear_right_speed * 1023)
--- tests/MoveTest.py
+++ tests/MoveTest.py
@@ -16,44 +16,47 @@ if __name__ == '__main__':
     #linear_velocity = (1.0, 0.0)  # движение вперед по оси X
     #angular_velocity = 0.0         # без вращения вокруг оси Z
 
+    #linear_velocity = (1.0, 0.0)  # движение вперед по оси X
+    #angular_velocity = 0.0         # без вращения вокруг оси Z
+
     #forward
-    adamController.Move((1.0, 0.0), 0.0)
-    time.sleep(0.01)
+    adamController.Move((0.0, 1.0), 0.0)
+    time.sleep(0.05)
 
     #back
-    adamController.Move((-1.0, 0.0), 0.0)
-    time.sleep(0.01)
+    adamController.Move((0.0, -1.0), 0.0)
+    time.sleep(0.05)
 
     #right
-    adamController.Move((0.0, -1.0), 0.0)
-    time.sleep(0.01)
+    adamController.Move((1.0, 0.0), 0.0)
+    time.sleep(0.05)
 
     #left
-    adamController.Move((0.0, 1.0), 0.0)
-    time.sleep(0.01)
+    adamController.Move((-1.0, 0.0), 0.0)
+    time.sleep(0.05)
 
     #left and forward
-    adamController.Move((1.0, 1.0), 0.0)
-    time.sleep(0.01)
+    adamController.Move((-1.0, 1.0), 0.0)
+    time.sleep(0.05)
 
     #rigt and forward
-    adamController.Move((1.0, -1.0), 0.0)
-    time.sleep(0.01)
+    adamController.Move((1.0, 1.0), 0.0)
+    time.sleep(0.05)
 
     #back and left
-    adamController.Move((-1.0, 1.0), 0.0)
-    time.sleep(0.01)
+    adamController.Move((-1.0, -1.0), 0.0)
+    time.sleep(0.05)
 
     #back and right
-    adamController.Move((-1.0, -1.0), 0.0)
-    time.sleep(0.01)
+    adamController.Move((1.0, -1.0), 0.0)
+    time.sleep(0.05)
 
     #u-turn to the right
-    adamController.Move((0.0, 0.0), -1.0)
-    time.sleep(0.01)
+    adamController.Move((0.0, 0.0), 1.0)
+    time.sleep(0.05)
 
     #u-turn to the left
-    adamController.Move((0.0, 0.0), 1.0)
-    time.sleep(0.01)
+    adamController.Move((0.0, 0.0), -1.0)
+    time.sleep(0.05)
 
-    adamController.Move((0.0, 0.0), 0)
+    adamController.Move((0.0, 0.0), 0.0)
