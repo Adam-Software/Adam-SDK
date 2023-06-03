aa47913f947bf65a5e01a99a7c27946644d0a436
Editscode <38367082+Editscode@users.noreply.github.com>
2023-06-03 12:41:00 +0300

    Update Joint.py

--- Models/Joint.py
+++ Models/Joint.py
@@ -1,12 +1,12 @@
 class Joint:
-    lover_limit: int
+    lower_limit: int
     upper_limit: int
     speed: int
     servo_Id: int
     id: int
     
-    def __init__(self, lover_limit: int, upper_limit: int, speed: int, id: int) -> None:
-        self.lover_limit = lover_limit
+    def __init__(self, lower_limit: int, upper_limit: int, speed: int, id: int) -> None:
+        self.lower_limit = lower_limit
         self.upper_limit = upper_limit
         self.speed = speed
         self.id = id
